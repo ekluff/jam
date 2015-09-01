3a983f679c6db2ec3fd7c9db886a338f550782f6
Evan Clough & Andrew Haupt <student@epicodus.com>
2015-09-01 10:40:25 -0700

    Add basic css styling.

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ Bundler.require :default
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+require("sinatra")
+require("sinatra/reloader")
+
+
 before do
   cache_control :public, :no_cache
 	cache_control :views, :no_cache
