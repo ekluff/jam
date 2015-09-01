d82baed8b317e497c0501fa2b737f51e12657ed4
Jake Glassman & Alexa Zeazas <student@epicodus.com>
2015-09-01 14:12:23 -0700

    Add validations to Session class, pass.

--- lib/session.rb
+++ lib/session.rb
@@ -1,4 +1,11 @@
 class Session < ActiveRecord::Base
   belongs_to(:user)
   belongs_to(:instrument)
+  validates(:address, :presence => true)
+  validates(:city, {:presence => true, :format => { :with => /\A[A-Za-z\s]+\z/, :message => "only allows letters" }})
+  validates(:state, {:presence => true, :length => { :is => 2 }, :format => { :with => /\A[A-Z]+\z/, :message => "only allows capital letters" }})
+  validates(:zip, {:presence => true, :length => { :is => 5 }, :format => { :with => /\A\d+\z/, :message => "only allows numbers" }})
+  validates(:date, :presence => true)
+  validates(:time, :presence => true)
+  validates(:host :presence => true)
 end
--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -3,4 +3,17 @@ require('spec_helper')
 describe(Session) do
   it { should belong_to(:user) }
   it { should belong_to(:instrument) }
+
+  it { should validate_presence_of(:address) }
+  it { should validate_presence_of(:city) }
+  it { should validate_presence_of(:state) }
+  it { should validate_presence_of(:zip) }
+  it { should validate_length_of(:state).is_equal_to(2) }
+  it { should validate_length_of(:zip).is_equal_to(5) }
+  it { should allow_value("Culver City").for(:city) }
+  it { should_not allow_value("Culv3r City").for(:city).with_message("only allows letters") }
+  it { should allow_value("OR").for(:state) }
+  it { should_not allow_value("Oregon").for(:state).with_message("only allows capital letters") }
+  it { should allow_value("92342").for(:zip) }
+  it { should_not allow_value("hjkdah").for(:zip).with_message("only allows numbers") }
 end
