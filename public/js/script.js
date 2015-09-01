1d8192ac57036c60521d497758a187f75faea89d
Evan Clough & Andrew Haupt <student@epicodus.com>
2015-09-01 11:10:57 -0700

    Add script and css.

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+//
+//
+// $(document).ready(function(){
+//   $('.scrollspy').scrollSpy();
+// });
+
+var main = function(){
+  $('.scrollspy').scrollSpy();
+  console.log("Hello World");
+};
+
+$(document).ready(main);
