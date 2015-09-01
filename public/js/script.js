c90ba3832853e7a5970c7d9e5cecde0040959e3b
Evan Clough & Andrew Haupt <student@epicodus.com>
2015-09-01 14:37:17 -0700

    Add javavscript and fix second navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,21 @@
-//
-//
-// $(document).ready(function(){
-//   $('.scrollspy').scrollSpy();
-// });
-
 var main = function(){
+
   $('.scrollspy').scrollSpy();
   console.log("Hello World");
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() <= $('.icon-nav').height()) {
+      $('.icon-nav').removeClass('fix-icon-nav')
+    } else {
+      $('.icon-nav').addClass('fix-icon-nav')
+    };
+  });
+
 };
 
 $(document).ready(main);
+
+
+
+// animate({top: 0, left: 0, right: 0}, 500)
+// $('.icon-nav').css({"position":"fixed"})
