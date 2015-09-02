5c4aa272d38e8cf3f39bfedf36fc72ced47265c5
Andrew Haupt <ajhaupt7@gmail.com>
2015-09-01 22:36:18 -0700

    Write bcrypt methods for user authentication

--- app.rb
+++ app.rb
@@ -3,8 +3,24 @@ Bundler.require :default
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
-require("sinatra")
-require("sinatra/reloader")
+enable :sessions
+set :session_secret, 'womb@tduckp!gcow'
+
+helpers do
+
+  def login?
+    if session[:username].nil?
+      return false
+    else
+      return true
+    end
+  end
+
+  def user
+    return session[:user]
+  end
+
+end
 
 
 before do
@@ -14,4 +30,35 @@ end
 
 get '/' do
   erb(:index)
+end
+
+post '/signup' do
+  name = params.fetch('name')
+  email = params.fetch('email')
+  password = params.fetch('password')
+  password_confirmation = params.fetch('password_confirmation')
+  password_hash = BCrypt::Password.create(password)
+  @user = User.create({:name => name, :email => email, :password => password_hash})
+  if @user.save()
+    redirect "/user/#{@user.id}"
+  else
+    erb(:errors)
+  end
+end
+
+post '/login' do
+  email = params.fetch("email")
+  password = params.fetch("password")
+  @user = User.find_by(:email => email)
+  if @user.authenticate(password)
+    session[:username] = @user
+    redirect "/user/#{@user.id}"
+  else
+    erb(:errors)
+  end
+end
+
+get '/logout' do
+  session[:username] = nil
+  redirect('/')
 end
