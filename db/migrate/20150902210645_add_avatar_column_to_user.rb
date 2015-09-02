2d869978c59b212f6fbb4b1ecf7c7112dee30a49
Evan Clough <clough.evan@gmail.com>
2015-09-02 15:36:01 -0700

    Correct styling of section divs on index.erb.

--- db/migrate/20150902210645_add_avatar_column_to_user.rb
+++ db/migrate/20150902210645_add_avatar_column_to_user.rb
@@ -1,11 +1,10 @@
 class AddAvatarColumnToUser < ActiveRecord::Migration
   def change
-    def change
-      def self.up
-      change_table :users do |t|
-        t.has_attached_file :avatar
-      end
+    def self.up
+    change_table :users do |t|
+      t.has_attached_file :avatar
     end
+  end
 
     def self.down
       drop_attached_file :users, :avatar
--- lib/user.rb
+++ lib/user.rb
@@ -3,9 +3,10 @@ require('pry')
 
 class User < ActiveRecord::Base
   include Paperclip::Glue
+  attr_accesible :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   has_many(:sessions)
   has_many(:instruments, :through => :sessions)
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }
   validates(:first_name, {:presence => true, :format => { :with => /\A[a-zA-Z\s\-]+\z/, :message => "only allows letters" }})
   validates(:last_name, {:presence => true, :format => { :with => /\A[a-zA-Z\s\-]+\z/, :message => "only allows letters" }})
   validates(:username, {:presence => true, :uniqueness => true, :length => { :minimum => 6, :maximum => 15 }, :format => { :with => /\A\w+\z/, :message => "only allows letters, numbers and underscore" }})
@@ -24,6 +25,10 @@ class User < ActiveRecord::Base
     else
       return false
     end
+  end
+
+  def create
+    @user = User.create( params[:user] )
   end
 
   private
