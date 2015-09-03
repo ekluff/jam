107229d965d82c4a9833d68878fd50fcd6587e6e
Andrew Haupt <ajhaupt7@gmail.com>
2015-09-03 10:13:21 -0700

    add dropdown to navbar

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,16 @@ var main = function(){
     };
   });
 
+  $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true // Displays dropdown below the button
+    }
+  );
+
   $('select').material_select();
 
   $('#fullpage').fullpage();
